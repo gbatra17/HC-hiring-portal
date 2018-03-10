f8acc7bc972434fde6758df3fc01cb8a5b2d3b02
gbatra17 <gbatra17@gmail.com>
2018-03-10 16:55:53 -0600

    Edit Filepicker

--- client/src/index.js
+++ client/src/index.js
@@ -9,8 +9,7 @@ class App extends Component {
     return ( <div>
       <ReactFilestack apikey = {process.env.FILEPICKER_API_KEY}
       buttonText = "Click me"
-      buttonClass = "classname"
-      options = {options}/>
+      buttonClass = "classname"/>
       <h2 > Hiring portal for HolaCode students < /h2>
       <h3 > Made using FileStack < /h3> <
       /div>);
