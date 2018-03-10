83afb224ee9fbc4521974a50c6c88e9fad56b97b
gbatra17 <gbatra17@gmail.com>
2018-03-10 16:47:20 -0600

    Added React FileStack

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,20 @@
-import React, {Component} from 'react';
+import React, {
+  Component
+} from 'react';
 import ReactDOM from 'react-dom';
+import ReactFilestack from 'filestack-react';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-      <h2>Hiring portal for HolaCode students</h2>
-      <h3>Made using FileStack</h3>
-    </div>);
+    return ( <div>
+      <ReactFilestack apikey = {process.env.FILEPICKER_API_KEY}
+      buttonText = "Click me"
+      buttonClass = "classname"
+      options = {options}/>
+      <h2 > Hiring portal for HolaCode students < /h2>
+      <h3 > Made using FileStack < /h3> <
+      /div>);
+    }
   }
-}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  ReactDOM.render( < App / > , document.getElementById('root'));
