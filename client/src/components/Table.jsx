9d89bf412d0c70a072524f6aabb360054e315c28
gbatra17 <gbatra17@gmail.com>
2018-03-13 02:10:06 -0600

    Added dummy data table

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import ReactFilestack from 'filestack-react';
 import Paper from 'material-ui/Paper';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
 
-const style = {
-  height: 500,
-  width: 500,
-  margin: 20,
-  textAlign: 'center',
-  display: 'inline-block',
-};
-
-const Table = () => (
+const TableView = () => (
   <div>
-  <h2>This is where the table goes</h2>
-</div>
+    <Table selectable={false}>
+      <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
+        <TableRow showRowHover={true} displayRowCheckbox={false}>
+          <TableHeaderColumn>Name Of Company</TableHeaderColumn>
+          <TableHeaderColumn>Date Of Post</TableHeaderColumn>
+          <TableHeaderColumn>Preview</TableHeaderColumn>
+          <TableHeaderColumn>Download</TableHeaderColumn>
+        </TableRow>
+      </TableHeader>
+      <TableBody displayRowCheckbox={false}>
+        <TableRow showRowHover={true} >
+          <TableRowColumn>Icani Labs</TableRowColumn>
+          <TableRowColumn>12/3/2018</TableRowColumn>
+          <TableRowColumn>Preview Coming</TableRowColumn>
+          <TableRowColumn>Download Coming</TableRowColumn>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </div>
 );
 
-export default Table;
+export default TableView;
